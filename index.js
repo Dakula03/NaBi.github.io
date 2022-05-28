3b6049e71d552196440dbd9ec25b34838288df01
Dakula03 <96925117+Dakula03@users.noreply.github.com>
2022-05-28 19:58:23 +0600

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-let test = document.getElementById("about");
-test.addEventListener("mouseover", function( event ) {
-  event.target.style.color = "grey";
-  setTimeout(function() {
-    event.target.style.color = "";
-  }, 500);
-}, false);
